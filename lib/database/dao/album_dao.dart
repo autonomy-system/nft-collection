2dd5d3507b90d08b7f755605c5084ca8073a3570
PuPha <nguyensanghso@gmail.com>
2023-09-28 14:23:16 +0700

    Update getAlbumsByMedium

--- lib/database/dao/album_dao.dart
+++ lib/database/dao/album_dao.dart
@@ -34,7 +34,7 @@ class AlbumDao {
 
   final QueryAdapter _queryAdapter;
   Future<List<AlbumModel>> getAlbumsByArtist({String name = ""}) async {
-    final nameFilter = "%${name}%";
+    final nameFilter = "%$name%";
     return _queryAdapter.queryList(
       'SELECT count(Token.id) as total, artistID as id, artistName as name, Asset.galleryThumbnailURL as  thumbnailURL FROM Token LEFT JOIN Asset  ON Token.indexID = Asset.indexID JOIN AddressCollection ON Token.owner = AddressCollection.address WHERE name LIKE ?1 AND AddressCollection.isHidden = FALSE GROUP BY artistID ORDER BY total DESC',
       mapper: mapper,
@@ -42,12 +42,62 @@ class AlbumDao {
     );
   }
 
-  Future<List<AlbumModel>> getAlbumsByMedium({String title = ""}) async {
-    final titleFilter = "%${title}%";
+  Future<List<AlbumModel>> getAlbumsByMedium(
+      {String title = "", required String mediumCategory}) async {
+    final titleFilter = "%$title%";
+    final mineTypes = MediumCategory.mineTypes(mediumCategory);
+    final mimeTypeFilter = "(${mineTypes.map((e) => "'$e'").join(",")})";
+    final albumId = mediumCategory;
     return _queryAdapter.queryList(
-      'SELECT count(Token.id) as total, medium as id, medium as name, Asset.galleryThumbnailURL as  thumbnailURL FROM Token LEFT JOIN Asset  ON Token.indexID = Asset.indexID JOIN AddressCollection ON Token.owner = AddressCollection.address WHERE Asset.title LIKE ?1 AND AddressCollection.isHidden = FALSE GROUP BY medium ORDER BY total DESC',
+      'SELECT count(Token.id) as total, ?3 as id, ?3 as name, Asset.galleryThumbnailURL as  thumbnailURL FROM Token LEFT JOIN Asset  ON Token.indexID = Asset.indexID JOIN AddressCollection ON Token.owner = AddressCollection.address WHERE Asset.title LIKE ?1 AND AddressCollection.isHidden = FALSE AND mimeType IN ?2 ORDER BY total DESC',
       mapper: mapper,
-      arguments: [titleFilter],
+      arguments: [titleFilter, mimeTypeFilter, albumId],
     );
+  }
+}
+
+class MediumCategory {
+  static const image = "image";
+  static const video = "video";
+  static const model = "model";
+  static const webView = "webView";
+  static const other = "other";
+
+  static List<String> mineTypes(String category) {
+    switch (category) {
+      case MediumCategory.image:
+        return [
+          "image/avif",
+          "image/bmp",
+          "image/jpeg",
+          "image/jpg",
+          "image/png",
+          "image/tiff",
+          "image/svg+xml",
+          "image/gif",
+        ];
+      case MediumCategory.video:
+        return [
+          "video/x-msvideo",
+          "video/3gpp",
+          "video/mp4",
+          "video/mpeg",
+          "video/ogg",
+          "video/3gpp2",
+          "video/quicktime",
+          "application/x-mpegURL",
+          "video/x-flv",
+          "video/MP2T",
+          "video/webm",
+          "application/octet-stream",
+        ];
+      case MediumCategory.model:
+        return ['model/gltf-binary'];
+      case MediumCategory.webView:
+        return ['text/html'];
+      case MediumCategory.other:
+        return [];
+    }
+    return [];
   }
 }
--- lib/models/album_model.dart
+++ lib/models/album_model.dart
@@ -1,8 +1,8 @@
 class AlbumModel {
-  final String id;
-  final String? name;
-  final int total;
-  final String? thumbnailURL;
+  String id;
+  String? name;
+  int total;
+  String? thumbnailURL;
   AlbumModel({
     required this.id,
     this.name,
