adbf117dc53ffbc2a7c9eda4dd5a1aeec573cc06
PuPha <nguyensanghso@gmail.com>
2024-09-10 11:20:46 +0700

    update tzkt api

--- lib/data/api/tzkt_api.dart
+++ lib/data/api/tzkt_api.dart
@@ -17,9 +17,11 @@ abstract class TZKTApi {
 
   @GET("/v1/tokens/transfers")
   Future<List<TZKTTokenTransfer>> getTokenTransfer({
-    @Query("to.in") required String to,
-    @Query("sort.desc") String sort = "timestamp",
-    @Query("token.tokenId.in") String? tokenIds,
-    @Query("select") String? select,
+    @Query("anyof.from.to") String? anyOf,
+    @Query("to") String? to,
+    @Query("sort.desc") String sort = "id",
+    @Query("limit") int? limit,
+    @Query("lastId") int? lastId,
+    @Query("timestamp.gt") String? lastTime,
   });
 }
